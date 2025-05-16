50d0d6f0549c91560ed11c1dd918b8438d9f1231
ZheXian <84336243+oncelxlc@users.noreply.github.com>
2025-05-16 11:39:31 +0800

    build(zx-tauri):升级 Angular 版本并配置 SCSS
    - 将 Angular版本从 18.2 升级到 19.2
    - 将样式文件从 CSS 改为 SCSS
    - 更新项目配置，支持 SCSS样式
    - 修改组件样式文件扩展名

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { invoke } from "@tauri-apps/api/core";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
   greetingMessage = "";
